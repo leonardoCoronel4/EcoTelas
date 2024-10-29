9afcd09ad1a6da43da93ab3ec9286a0bd261e286
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-10-28 21:44:53 -0300

    [Contact] page

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../assets/styles/App.css';
 import NewUser from '../pages/user/new';
 import RecyclingPoints from '../pages/recyclingPoints';
+import Contact from '../pages/contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppMain from './index';
 import NavBar from '../components/NavBar';
@@ -23,6 +24,10 @@ const App = () => {
                     <Route
                         path="/recycling-points"
                         element={<RecyclingPoints />}
+                    />
+                    <Route
+                        path="/contact"
+                        element={<Contact />}
                     />
                 </Routes>
             </div>
